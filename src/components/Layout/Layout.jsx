0ededd6829c24be05304df654bf5dd3a1b5c3b33
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-29 01:34:11 +0200

    Update Task component

--- src/components/Layout/Layout.jsx
+++
@@ -1,7 +0,0 @@
-import css from './Layout.module.css';
-
-const Layout = ({ children }) => {
-  return <main className={css.container}>{children}</main>;
-};
-
-export default Layout;
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,5 +1,4 @@
 import { useDispatch } from 'react-redux';
-import { deleteTask, toggleCompleted } from '../../redux/tasksSlice';
 import { MdClose } from 'react-icons/md';
 import css from './Task.module.css';
 
@@ -7,28 +6,15 @@ const Task = ({ task }) => {
   //  Отримуємо посилання на функцію відправки екшенів
   const dispatch = useDispatch();
 
-  //  Викликаємо фабрику екшену та передаємо ідентифікатор завдання
-  //  Відправляємо результат - екшен видалення завдання
-  const handleDelete = () => {
-    dispatch(deleteTask(task.id));
-  };
-
-  //  Викликаємо фабрику екшену та передаємо ідентифікатор завдання
-  //  Відправляємо результат - екшен перемикання статусу завдання
-  const handleToggle = () => {
-    dispatch(toggleCompleted(task.id));
-  };
-
   return (
     <div className={css.wrapper}>
       <input
         className={css.checkbox}
         type="checkbox"
         checked={task.completed}
-        onChange={handleToggle}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn} onClick={handleDelete}>
+      <button className={css.btn}>
         <MdClose size={24} />
       </button>
     </div>
