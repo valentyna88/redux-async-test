e01d2fd309f939e49f0fbfe540a5fda6a68191dc
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-29 01:15:50 +0200

    Update operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -8,9 +8,20 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 
-export const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/tasks');
-
-  return response.data;
-});
+export const fetchTasks = createAsyncThunk(
+  'tasks/fetchAll',
+  // Використовуємо символ підкреслення як ім'я першого параметра,
+  // тому що в цій операції він нам не потрібен
+  async (_, thunkApi) => {
+    try {
+      const response = await axios.get('/tasks');
+      // При успішному запиті повертаємо проміс із даними
+      return response.data;
+    } catch (e) {
+      // При помилці запиту повертаємо проміс
+      // який буде відхилений з текстом помилки
+      return thunkApi.rejectWithValue(e.message);
+    }
+  }
+);
 // export default fetchTasks;
