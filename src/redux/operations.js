3fe57c1af67aae4beed392f326673c5c40ebd3eb
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-29 00:43:53 +0200

    Update operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,22 +1,16 @@
 import axios from 'axios';
-import {
-  fetchingInProgress,
-  fetchingSuccess,
-  fetchingError,
-} from './tasksSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+// import {
+//   fetchingInProgress,
+//   fetchingSuccess,
+//   fetchingError,
+// } from './tasksSlice';
 
 axios.defaults.baseURL = 'https://62584f320c918296a49543e7.mockapi.io';
 
-const fetchTasks = () => async dispatch => {
-  try {
-    // Індикатор завантаження
-    dispatch(fetchingInProgress());
-    // HTTP-запит
-    const response = await axios.get('/tasks');
-    // Обробка даних
-    dispatch(fetchingSuccess(response.data));
-  } catch (e) {
-    // Обробка помилки
-    dispatch(fetchingError(e.message));
-  }
-};
+const fetchTasks = createAsyncThunk('tasks/fetchAll', async () => {
+  const response = await axios.get('/tasks');
+
+  return response.data;
+});
+export default fetchTasks;
