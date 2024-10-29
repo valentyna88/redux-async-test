1f1198651d901341715327491f7e3dfd052be4d8
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-29 12:37:46 +0200

    Add isLoading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTasks } from '../../redux/operations';
+import { getError, getIsLoading } from '../../redux/selectors';
 import AppBar from '../AppBar/AppBar';
 import TaskForm from '../TaskForm/TaskForm';
 import TaskList from '../TaskList/TaskList';
 import css from './App.module.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <AppBar />
       <TaskForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <TaskList />
     </div>
   );
