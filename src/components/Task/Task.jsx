e14482d044af5aba30eff3d9d7704d304daebd40
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-29 17:15:59 +0200

    Add processing of delete task actions

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,13 @@
 import { useDispatch } from 'react-redux';
 import { MdClose } from 'react-icons/md';
+import { deleteTask } from '../../redux/operations';
 import css from './Task.module.css';
 
 const Task = ({ task }) => {
   //  Отримуємо посилання на функцію відправки екшенів
   const dispatch = useDispatch();
+
+  const handleDelete = () => dispatch(deleteTask(task.id));
 
   return (
     <div className={css.wrapper}>
@@ -14,7 +17,7 @@ const Task = ({ task }) => {
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+      <button onClick={handleDelete} className={css.btn}>
         <MdClose size={24} />
       </button>
     </div>
