6b2b81c83e69c75d71b691b3e5ce87a7dfe7a023
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-10-30 09:29:57 +0200

    Add selectVisibleTasks selector

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,34 +1,18 @@
 import { useSelector } from 'react-redux';
-import { selectTasks, selectStatusFilter } from '../../redux/selectors';
-import { statusFilters } from './constants';
+import { selectVisibleTasks } from '../../redux/selectors';
 import Task from '../Task/Task';
 import css from './TaskList.module.css';
 
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
-  }
-};
-
 const TaskList = () => {
-  const tasks = useSelector(selectTasks);
-  const statusFilter = useSelector(selectStatusFilter);
-  // Обчислюємо масив завдань, які необхідно відображати в інтерфейсі
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
-  return (
-    <ul className={css.list}>
-      {visibleTasks.map(task => (
-        <li className={css.listItem} key={task.id}>
-          <Task task={task} />
-        </li>
-      ))}
-    </ul>
-  );
+  const tasks = useSelector(selectVisibleTasks);
+
+  <ul className={css.list}>
+    {tasks.map(task => (
+      <li className={css.listItem} key={task.id}>
+        <Task task={task} />
+      </li>
+    ))}
+  </ul>;
 };
 
 export default TaskList;
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { statusFilters } from './constants';
+
 export const selectTasks = state => state.tasks.items;
 
 export const selectIsLoading = state => state.tasks.isLoading;
@@ -5,3 +7,18 @@ export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 
 export const selectStatusFilter = state => state.filters.status;
+
+export const selectVisibleTasks = state => {
+  // Використовуємо інші селектори
+  const tasks = selectTasks(state);
+  const statusFilter = selectStatusFilter(state);
+
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
